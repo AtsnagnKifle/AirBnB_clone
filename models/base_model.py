2605dbd3632c66530bcb1b7c72aa5839c0d20fb7
atsnagnkifle <kifleatsnagn@gmail.com>
2022-08-07 23:33:09 +0300

    create basemodel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,33 @@ class BaseModel:
         Base Class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             Base model constructor
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at",
+                          None) and isinstance(self.created_at, str):
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at",
+                          None) and isinstance(self.updated_at, str):
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs["id"] is None:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
